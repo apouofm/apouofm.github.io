33d37dd016492477031f884cbec1b2ce885ac828
Evan Geiss <egeiss@umich.edu>
2024-08-29 15:55:38 -0400

    hamburger menu ayo lets go

---
+++ pages/pages.js
@@ -0,0 +1,30 @@
+class GlobalHeader extends HTMLElement {
+  constructor() {
+      super();
+      console.info('constructed');
+  }
+
+  connectedCallback() {
+      console.info('connected');
+
+      let self = this;
+      $.get("./subNav.html", function(data) {
+          let context = $(data).find("#navBar").html();
+          self.innerHTML = data;
+
+          // Add the hamburger menu functionality after the navbar is loaded
+          const navbarBurger = document.querySelector('.navbar-burger');
+          if (navbarBurger) {
+              const navbarMenu = document.getElementById(navbarBurger.dataset.target);
+              navbarBurger.addEventListener('click', () => {
+                  navbarBurger.classList.toggle('is-active');
+                  if (navbarMenu) {
+                      navbarMenu.classList.toggle('is-active');
+                  }
+              });
+          }
+      });
+  }
+}
+
+customElements.define('global-header', GlobalHeader);
